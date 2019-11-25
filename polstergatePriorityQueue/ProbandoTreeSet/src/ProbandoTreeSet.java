513732cd17d3ab3a7042cadeeb5a35c89fbc8746
Borja Badía <44509208+Borja92@users.noreply.github.com>
2019-11-25 14:16:03 +0100

    polstergatePriorityQueue

---
+++ polstergatePriorityQueue/ProbandoTreeSet/src/ProbandoTreeSet.java
@@ -0,0 +1,24 @@
+import java.util.TreeSet;
+
+public class ProbandoTreeSet {
+
+
+    public static void main(String[] args) {
+
+
+       // TreeSet<Tarea> tareas= new TreeSet<>(Tarea.COMPARATOR_PRIORIDAD);
+        TreeSet<Tarea> tareas= new TreeSet<>(Tarea.DESCRIPCION_TAREA );
+
+tareas.add(new Tarea(3,"B"));
+tareas.add(new Tarea(1,"A"));
+tareas.add(new Tarea(2,"C"));
+tareas.add(new Tarea(5,"Z"));
+tareas.add(new Tarea(1,"Y"));
+
+
+        System.out.println(tareas);
+    }
+
+
+
+}
